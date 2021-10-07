2eefccbddbe211b223b3ab6160329bcf9b745d4f
taoista <taoista-@hotmail.com>
2021-10-07 08:29:53 -0300

    actualizacion del proyecto tarea

--- tarea_1/main.py
+++ tarea_1/main.py
@@ -4,39 +4,47 @@ estado = True
 intentos = 0
 puntos = 0
 
-numero = random.randrange(1,101)
+numero = random.randrange(1,3)
 
 get_nombre = input("Ingresa tu nombre: \n")
 
 while estado == True:
     intentos = intentos + 1
-    get_intento = int(input("Ingresa un numero que crees que es (0 para salir)\n"))
-    if get_intento == 0:
-        estado = False
-    else:
-        if numero == get_intento:
-            print("Felicitaciones ",get_nombre, "lo lograste en ", intentos, " intentos")
+    get_intento = input("Ingresa un numero que crees que es (0 para salir)\n")
+    if get_intento.isdigit():
+        
+        if get_intento == 0:
             estado = False
         else:
-            if get_intento < numero :
-                if ( numero - 5 ) <= get_intento:
-                    print("Sorry ",get_nombre, "ese no es pero estas a una distancia menor a 5")
-                elif ( numero - 10) <= get_intento:
-                    print("Sorry ",get_nombre, "ese no es pero estas a una distancia menor a 10")
-                elif ( numero - 20) <= get_intento:
-                    print("Sorry ",get_nombre, "ese no es pero estas a una distancia menor a 20")
-                else:
-                    print("sigue siendo menor")
+            get_intento = int(get_intento)
+            if numero == get_intento:
+                print(f'Felicitaciones {get_nombre} lo lograste en {intentos} intentos')
+                estado = False
             else:
-                if (numero + 5) >= get_intento:
-                    print("Sorry ",get_nombre, "ese no es pero estas a una distancia mayor a 5")
-                elif (numero + 10) >= get_intento:
-                    print("Sorry ",get_nombre, "ese no es pero estas a una distancia mayor a 10")
-                elif (numero + 20) >= get_intento:
-                    print("Sorry ",get_nombre, "ese no es pero estas a una distancia mayor a 20")
+                if get_intento < numero :
+                    if ( numero - 5 ) <= get_intento:
+                        print(f"Sorry ,{get_nombre}, ese no es pero estas a una distancia menor a 5")
+                    elif ( numero - 10) <= get_intento:
+                        print(f"Sorry {get_nombre}, ese no es pero estas a una distancia menor a 10")
+                    elif ( numero - 20) <= get_intento:
+                        print(f"Sorry {get_nombre}, ese no es pero estas a una distancia menor a 20")
+                    else:
+                        print("sigue siendo menor")
                 else:
-                    print("es mucho mayor")
+                    if (numero + 5) >= get_intento:
+                        print(f"Sorry {get_nombre} ese no es pero estas a una distancia mayor a 5")
+                    elif (numero + 10) >= get_intento:
+                        print(f"Sorry ,{get_nombre}, ese no es pero estas a una distancia mayor a 10")
+                    elif (numero + 20) >= get_intento:
+                        print(f"Sorry ,{get_nombre}, ese no es pero estas a una distancia mayor a 20")
+                    else:
+                        print("es mucho mayor")
 
-# while estado = True:
+    else:
+        print("no es numero")
+
+
+
+
 
     
