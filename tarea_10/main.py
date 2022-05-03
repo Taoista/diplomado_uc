61ccf7f6bde39054819f6b0c1e22808354bc3474
Taoista <taoista-@hotmail.com>
2022-05-03 05:04:03 -0400

    actualzicion del proyecto

--- tarea_10/main.py
+++ tarea_10/main.py
@@ -13,42 +13,52 @@ def main():
     mydb = client["mongo"]
     colect = mydb["feriados2020"]
     # print(mydb.list_collection_names())
-    
+    colect_all = mydb["feriadosAll"]
 
     #feriados_2022
-    response = requests.get("https://apis.digital.gob.cl/fl/feriados/2022",headers={
+    response = requests.get("https://apis.digital.gob.cl/fl/feriados/2020",headers={
+        "User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
+    })
+
+    response_all = requests.get("https://apis.digital.gob.cl/fl/feriados",headers={
         "User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
     })
 
     data = response.json()
-
+    data_all = response_all.json()
     ##### 
     ##### inserta todos los datos del 2022   
     #####
     # colect.insert_many(data)
-
+    # colect_all.insert_many(data_all)
+    # print("insert completo")
     ##### 
     ##### siguientes consultas
     #####
 
     ##### muestra todo
+    print("===== Todos los Feriados de 2020 ====")
     for i in colect.find({}):
-        # print(i)
+        # print(f"El día de {i['nombre']} es un feriado de tipo {i['tipo']} y se celebra el {i['fecha']}")
         pass
 
-    ##### obtener solo los feriados civiles
+    print("===== Solo los Feriados Civiles de 2020 =====")
     for i in colect.find({"tipo":"Civil" }):
-        # print(i)
+        # print(f"El día de Todos {i['nombre'] } es un feriado de tipo {i['tipo']} y se celebra el {i['fecha']}")
         pass
-    ##### obtener solo los feriados irrenunciables
-    for i in colect.find({"irrenunciable":1 }):
-        # print(i)
+
+    print("===== Solo los Feriados Irrenunciables de 2020 =====")
+    for i in colect.find({"irrenunciable":"1" }):
+        # print(f"El día de {i['nombre']} es un feriado de tipo {i['tipo']} y se celebra el {i['fecha']}")
         pass
-    ##### obtener solo los feriados que incluyen el texto "Santo" en el nombre
-    for i in colect.find({
-        "$regex":
-    }):
+    print("===== Solo los Feriados que incluyen 'Santo' o 'Santos' =====")
+    for i in colect.find({"nombre":{"$regex":"\w*Santo\w*"}},{"_id":0}):
+        # print(f"El día de {i['nombre']} es un feriado de tipo {i['tipo']} y se celebra el {i['fecha']}")
+        pass
+    ####  obtener las leyes involucradas en el feriado del Plebiscito 
+    for i in response_all.find({"comentarios":{"$regex":"\w*plebi\w*"}},{"_id":0}):
         print(i)
+        pass
 
 
     print("______terminando________")
