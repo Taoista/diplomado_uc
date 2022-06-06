227e6fba715488b8870f06e7505932899072c1fd
taoista <taoista-@hotmail.com>
2022-06-06 17:10:47 -0400

    actualizacion del proeycto

---
+++ tarea_11/main.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.decomposition import PCA
+
+
+# * id_cliente => df_bill_atm
+# * limit_credito => df_bill_atm
+# * nivel de educacion => df_card_pay_atm
+#  * sexo => df_bill_atm
+#  * nivel_educacion => df_card_pay_atm
+# * state_civil => df_card_paynext
+print("inicando")
