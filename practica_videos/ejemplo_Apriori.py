5abf04c71501ba60ee9f6a556e524ceb3852ad36
Taoista <taoista-@hotmail.com>
2022-07-02 11:26:37 -0400

    faltante

---
+++ practica_videos/ejemplo_Apriori.py
@@ -0,0 +1,18 @@
+# 
+# * ejemplo practicod e apriori
+# 
+
+from pyparsing import matchPreviousLiteral
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+from apyori import  apriori
+
+matplotlib notebook
+
+
+
+
+
---
+++ practica_videos/practica.py
@@ -0,0 +1,31 @@
+# * informacion video
+# Ejemplo PCA Principal Component Analisis
+#  tencnica de reduccion dimencionalidad lineal
+# vide
+# https://ecursos.claseejecutiva.cl/mod/page/view.php?id=1188450
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.decomposition import PCA
+
+
+
+def main():
+    # carga de archivo
+    df = pd.read_csv("")       
+    df.head()
+
+
+    #atributos 
+    df.columns
+
+
+if __name__ == "__main__":
+    main()
+
+
+#
+p
